4c481df6400557724e99f46177491e231a44d7c0
alessio <alessio.cirillo.1995@gmail.com>
2020-11-23 17:54:52 +0100

    vue & axios

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ const app = new Vue({
 
     methods:{
         getEmail(){
-            for(let i= 0; i= 10; i++){
+            for(let i= 0; i < 10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) => {
                     this.emails.push(response.data.response)
